96f2c2ad41719a8ea68cbcfaddc84a02904222ae
phucle1011 <phuc628780@gmail.com>
2025-03-17 16:54:21 +0700

    init

---
+++ src/app/layouts/full/sidebar/sidebar-data.ts
@@ -0,0 +1,67 @@
+import { NavItem } from './nav-item/nav-item';
+
+export const navItems: NavItem[] = [
+  {
+    navCap: 'Home',
+  },
+  {
+    displayName: 'Dashboard',
+    iconName: 'layout-grid-add',
+    route: '/dashboard',
+  },
+  {
+    navCap: 'Ui Components',
+  },
+  {
+    displayName: 'Categories',
+    iconName: 'adjustments-minus',
+    route: '/ui-components/categories',
+  },
+  {
+    displayName: 'Chips',
+    iconName: 'info-circle',
+    route: '/ui-components/chips',
+  },
+  {
+    displayName: 'Lists',
+    iconName: 'list-details',
+    route: '/ui-components/lists',
+  },
+  {
+    displayName: 'Forms',
+    iconName: 'clipboard-text',
+    route: '/ui-components/forms',
+  },
+  {
+    displayName: 'Tables',
+    iconName: 'table',
+    route: '/ui-components/tables',
+  },
+  {
+    navCap: 'Auth',
+  },
+  {
+    displayName: 'Login',
+    iconName: 'login',
+    route: '/authentication',
+    children: [
+      {
+        displayName: 'Login',
+        iconName: 'point',
+        route: '/authentication/login',
+      },
+    ],
+  },
+  {
+    displayName: 'Register',
+    iconName: 'user-plus',
+    route: '/authentication',
+    children: [
+      {
+        displayName: 'Register',
+        iconName: 'point',
+        route: '/authentication/register',
+      },
+    ],
+  },
+];
