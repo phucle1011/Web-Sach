6b39e137853c8aee1b350fb9ef6ba66f92288100
gialuan66666 <kioaoa2407@gmail.com>
2025-04-07 22:50:38 +0700

    Update client-layout.component.ts

--- src/app/client/layouts/client-layout/client-layout.component.ts
+++ src/app/client/layouts/client-layout/client-layout.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-client-layout',
   imports: [HeaderComponent,FooterComponent,RouterOutlet],
-=======
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-import { RouterModule } from '@angular/router';
-
-@Component({
-  selector: 'app-client-layout',
-  imports: [HeaderComponent,FooterComponent,RouterModule],
->>>>>>> 5d29765af0f756f67b11f865a3a3f514fbd68d35
   templateUrl: './client-layout.component.html',
   styleUrl: './client-layout.component.scss'
 })
