3ece57dedd15fe984e62e16592d7fddb34d20900
NHT05 <nhutnguyentrung449@gmail.com>
2025-04-11 12:58:46 +0700

    [Client] HOME

--- src/app/client/layouts/layout.components.routes.ts
+++ src/app/client/layouts/layout.components.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { CartComponent } from '../pages/cart/cart.component';
 import { ClientLayoutComponent } from './client-layout/client-layout.component';
 import { PaymentComponent } from '../pages/cart/payment/payment.component';
+import { HomeComponent } from '../pages/home/home.component';
 
 export const routesClient: Routes = [
   {
     path: '',
     component: ClientLayoutComponent,
     children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
       {
         path: 'cart',
         component: CartComponent,
