570b9ea4bdaf75336ea6931a5652e84b50e5850c
quivt4125 <vanquythaicute@gmail.com>
2025-04-17 00:55:15 +0700

    [admin] user, cách cách vài cái rồi đó nha
    (cherry picked from commit c07f176b841c553a183ef46c892a48ff6ad329f8)

--- BE/models/userModel.js
+++ BE/models/userModel.js
@@ -4,45 +4,46 @@ const connection = require('../database');
 
 
 const User = connection.define('User', {
-  userId: { 
-    type: DataTypes.INTEGER, 
-    autoIncrement: true, 
-    primaryKey: true 
+  userId: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
   },
-  name: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-  email: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-  password: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-  phoneNumber: { 
-    type: DataTypes.STRING(10), 
-    allowNull: false 
+  phoneNumber: {
+    type: DataTypes.STRING(10),
+    allowNull: false
   },
-  address: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-  createdAt: { 
-    type: DataTypes.DATE 
+  createdAt: {
+    type: DataTypes.DATE
   },
-  role: { 
-    type: DataTypes.ENUM('Admin', 'User'), 
-    allowNull: false 
+  role: {
+    type: DataTypes.ENUM('Admin', 'User'),
+    allowNull: false
   },
-  avatar: { 
-    type: DataTypes.STRING, 
-    allowNull: true 
+  avatar: {
+    type: DataTypes.STRING,
+    allowNull: true
   },
 }, {
   tableName: 'users',
   timestamps: false,
 });
+
 
 module.exports = User;
